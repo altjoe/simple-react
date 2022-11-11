ae04a70cd5a1b6af5fe96a6f4d9cb5deabd781b7
JoeMac <meyer.joer5@gmail.com>
2022-11-11 13:58:19 -0500

    working css fluid table need to work on input and onChange

--- src/components/Table/table.js
+++ src/components/Table/table.js
@@ -1,11 +1,47 @@
 import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types'
+import TextArea from 'react-expanding-textarea'
 
 export default function Table(props) {
-    return (
-        <div className={props.class}>Test</div>
-    );
+
+    const displayTable = () => {
+        if (props.data) {
+            console.log(typeof props.flex);
+            const rowindex = Object.keys(props.data)
+            if (rowindex.length > 0) {
+                const columns = Object.keys(props.data[0])
+                return (
+                    <div className={`${props.wrapperClass}`}>
+                        <div className={`${props.headerClass} ${props.rowClass}`}>
+                            {columns.map((col, i) => <div className={`${props.headerCellClass}`} 
+                                                        style={{'flex' : typeof props.flex === 'undefined' ? '' : props.flex[i]}}>{col}</div>)}
+                        </div>
+                        <div className={`${props.bodyClass}`}>
+                            {rowindex.map(i => {
+                                return (
+                                    <div className={`${props.rowClass}`}>
+                                        {columns.map(col => <TextArea className={`${props.cellClass} resize-none outline-none`} 
+                                                                rows={1}
+                                                                value={props.data[i][col]}
+                                                                style={{'flex' : typeof props.flex === 'undefined' ? '' : props.flex[i]}}/>)}
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                )
+            } 
+        }
+    }
+
+    return displayTable();
 }
 Table.propTypes = {
-    'class' : PropTypes.string,
+    'wrapperClass' : PropTypes.string,
+    'headerClass' : PropTypes.string,
+    'bodyClass' : PropTypes.string,
+    'rowClass' : PropTypes.string,
+    'cellClass' : PropTypes.string,
+    'data' : PropTypes.object,
+    'flex' : PropTypes.array
 }
--- src/components/Table/table.stories.js
+++ src/components/Table/table.stories.js
@@ -14,8 +14,35 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Table {...args} />;
 
-export const Primary = Template.bind({});
+export const normalTable = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  class : 'border bg-slate-500'
+normalTable.args = {
+  wrapperClass : 'border',
+  headerClass : 'bg-green-300 border-none text-center',
+  bodyClass : '',
+  rowClass : 'flex border-b',
+  cellClass : 'flex-1 text-center focus:bg-slate-200 hover:bg-slate-100',
+  data : {
+    0: {'col1' : '1', 'col2' : '2', 'col3' : '3'},
+    1: {'col1' : '2', 'col2' : '3', 'col3' : '4'},
+    2: {'col1' : '3', 'col2' : '4', 'col3' : '5'},
+    3: {'col1' : '4', 'col2' : '5', 'col3' : '6'},
+    4: {'col1' : '5', 'col2' : '6', 'col3' : '7'}
+  },
+  flex: [1, 1, 1]
+};
+
+export const sidewaysTable = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+sidewaysTable.args = {
+  wrapperClass : 'flex text-center border border-b-transparent',
+  headerClass : '',
+  bodyClass : 'flex flex-1',
+  rowClass : 'flex flex-col flex-1',
+  cellClass : 'flex-1 text-center focus:bg-slate-200 hover:bg-slate-200 border-b',
+  headerCellClass : 'border-b bg-purple-200',
+  data : {
+    0: {'row1' : '1', 'row2' : 2}
+  },
+  flex : [1, 1]
 };
