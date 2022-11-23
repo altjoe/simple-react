865c1ad77af49993fbd125152a891c5022f7c5e2
altjoe <meyer.joer5@gmail.com>
2022-11-22 20:23:43 -0500

    reworked table again, much simpler to understand almost ready to ship

--- src/components/SimpTable/simp-table.js
+++ src/components/SimpTable/simp-table.js
@@ -4,38 +4,109 @@ import TextArea from 'react-expanding-textarea'
 
 export default function SimpTable(props) {
 
-    const calculateInfo = () => {
-        return (Object.keys(props.data).map((key, i) => {
-                let row = props.data[key].map((val, j) => {
-                    return (
-                        <td className={`${props.inputContainerClass}`}>
-                            <div className={props.inputDisplayClass}>
-                                <TextArea className={props.inputClass} value={props.data[key][j]} />
-                            </div>
-                        </td>
-                    )
-                })
-                row.unshift(
-                    <th className={`${props.titleContainerClass}`}>
-                        <div className={props.titleDisplayClass}>{key}</div>
-                    </th>
-                )
+    const handleClick = (id) => {
+        const element = document.getElementById(id)
+        if (typeof element !== 'undefined') {
+            element.focus()
+            element.setSelectionRange(element.value.length, element.value.length)
 
+        }
+    }
+
+    const calculateRow = () => {
+        return Object.keys(props.data).map((key, i) => {
+            let row = props.data[key].map((val, j) => {
                 return (
-                    <tr className={`${props.rowClass} row`}>
-                        {row}
-                    </tr>
+                    <td className={`${props.inputContainerClass}`}>
+                        <div onClick={event => handleClick(`${key}-${j}`)} className={props.inputDisplayClass}>
+                            <TextArea id={`${key}-${j}`} className={props.inputClass} value={props.data[key][j]} />
+                        </div>
+                    </td>
                 )
             })
+            row.unshift(
+                <th className={`${props.titleContainerClass}`}>
+                    <div className={props.titleDisplayClass}>{key}</div>
+                </th>
+            )
+
+            return (
+                <tr className={`${props.rowClass} row`}>
+                    {row}
+                </tr>
+            )})
+            
+    }
+
+    const calculateColumn = () => {
+        return [calculateColumnHeader(), calculateColumnBody()]
+    }
+
+    const calculateColumnHeader = () => {
+        return (
+            <tr className={`${props.rowClass}`}>
+                {Object.keys(props.data).map((key, i) => {
+                    return (
+                        <th className={`${props.titleContainerClass}`} style={{'flex' : props.flex[i]}}>
+                            <div className={props.titleDisplayClass}>{key}</div>
+                        </th>
+                    )
+                })}
+            </tr>
         )
+    }
+
+    const calculateColumnBody = () => {
+        return props.data[Object.keys(props.data)[0]].map((_, j) => {
+            return (
+                <tr className={`${props.rowClass}`}>
+                    {Object.keys(props.data).map((key, i) => {
+                        return (
+                            <td className={`${props.inputContainerClass}`} style={{'flex' : props.flex[i]}}>
+                                <div onClick={event => handleClick(`${key}-${j}`)} className={`${props.inputDisplayClass}`}>
+                                    <TextArea id={`${key}-${j}`} className={`${props.inputClass}`} value={props.data[key][j]} />
+                                </div>
+                            </td>
+                        )
+                    })}
+                </tr>
+            )
+        })
+    }
+
+    const calculateInfo = () => {
+        if (typeof props.flexdirection !== 'undefined') {
+            switch (props.flexdirection.toLowerCase()) {
+                case 'row':
+                    return calculateRow()
+                    break;
+                case 'column':
+                    return calculateColumn()
+                    break;
+                default:
+                    console.log('flexdirection: \'${props.flexdirection}\' is not a accepted value -> default to row -> options (\'row\', \'column\')');
+                    break;
+            }
+        } else {
+            console.log('flexdirection is undefined -> default to row -> options (\'row\', \'column\')');
+            return calculateRow()
+        }
     }
 
     const displayTable = () => {
         if (props.data !== 'undefined') {
             return (
-                <table className={props.tableClass}>
-                    {calculateInfo()}
-                </table>
+                <>
+                    <table className={props.tableClass}>
+                        <tbody>
+                            {calculateInfo()}
+                        </tbody>
+                    </table>
+                    <div className={`${props.buttonContainerClass}`}>
+                        <button className={`${props.buttonClass}`}>Add</button>
+                    </div>
+                </>
+                
             )
         }
     }
--- src/components/SimpTable/simp-table.stories.js
+++ src/components/SimpTable/simp-table.stories.js
@@ -19,28 +19,49 @@ Horizontal.args = {
     rowClass : 'group flex flex-col sm:flex-row',
 
 
-    titleContainerClass : tw('flex flex-1 bg-green-200 rounded-lg', 
+    titleContainerClass : tw('md:w-1/5 flex bg-green-200 rounded-lg', 
                               'md:rounded-none md:group-first:rounded-t-lg md:group-last:rounded-b-lg'),
     titleDisplayClass : 'text-center self-center m-auto p-2',
 
 
     inputContainerClass : 'flex flex-1 border-b',
     inputDisplayClass : 'flex flex-1 hover:bg-red-200 rounded-lg ',
-    inputClass : 'flex-1 text-center resize-none bg-transparent outline-none px-2 py-1 self-center',
+    inputClass : 'flex-1 text-center resize-none bg-transparent outline-none px-2 py-1 self-center w-10',
 
-    // data : {
-    //   'test1' : [0, 1, 2, 3],
-    //   'test2' : [0, 1, 2, 3],
-    //   'test3' : [0, 1, 2, 3],
-    //   'test4' : [0, 1, 2, 3],
-    //   'test5' : [0, 1, 2, 3]
-    // }
     data : {
-      'description' : ['it would be extremely fucking annoying if this works', 1, 2, 3],
-      'Full Name' : ['Joe', 'Meyer'],
+      'Description' : ['it would be extremely fucking annoying if this works', 1, 2, 3],
+      'Full Name' : ['Joe', 'Ross', 'Meyer'],
+      'Row 3' : [0, 1, 2, 3],
+      'Row 4' : [0, 1, 2, 3]
+    }, 
+    flexdirection : 'row'
+};
+
+export const Vertical = Template.bind({});
+
+Vertical.args = {
+    tableClass : 'group w-full border-collapse caret-transparent',
+    rowClass : tw('flex overflow-hidden first:rounded-xl'),
+    titleContainerClass : tw('flex-1 bg-red-200 border-b border-white'),
+    titleDisplayClass : '',
+
+    inputContainerClass : 'flex flex-1 border-b',
+    inputDisplayClass : tw('flex flex-1 rounded-xl cursor-text', 
+                           'hover:bg-red-100', 
+                           'border-4 border-transparent focus-within:border-red-200 focus-within:bg-red-100'),
+    inputClass : 'flex-1 w-10 resize-none self-center text-center outline-none bg-transparent focus:caret-black ',
+
+    buttonContainerClass : 'flex w-full justify-center',
+    buttonClass : 'p-2 m-2 rounded-lg bg-green-200  caret-transparent',
+
+    data : {
+      'description' : ['it would be extremely fucking annoying if this worksit would be extremely fucking annoying if this worksit would be extremely fucking annoying if this worksit would be extremely fucking annoying if this worksit would be extremely fucking annoying if this works', 1, 2, 3],
+      'Full Name' : ['Joe', 'Ross', 'Meyer'],
       'row3' : [0, 1, 2, 3],
       'row4' : [0, 1, 2, 3]
-    }
+    },
+    flexdirection : 'row',
+    flex : [3, 1, 1, 1]
 };
 
 
